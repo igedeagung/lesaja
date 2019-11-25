5046202fba47db1deb3347e28752a3b9d911c49f
igedeagung <igedeagung25@gmail.com>
2019-11-25 10:51:07 +0700

    hehe

--- app/src/main/java/com/example/lesaja/RegisActivity.java
+++ app/src/main/java/com/example/lesaja/RegisActivity.java
@@ -1,5 +1,6 @@
 package com.example.lesaja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -10,13 +11,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class RegisActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGEEE = "hmm";
-
+    private DatabaseReference database;
+    private EditText editText;
+    private String pesan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +39,14 @@ public class RegisActivity extends AppCompatActivity {
     }
     public void Pindah(View view) {
         String message="+62";
-        EditText editText = (EditText)findViewById(R.id.editText5);
+        editText = (EditText)findViewById(R.id.editText5);
         String tambah = editText.getText().toString();
         final String hasil=message.concat(tambah);
         EditText editText2 = (EditText)findViewById(R.id.editText6);
         final String message2 = editText2.getText().toString();
         EditText editText3 = (EditText)findViewById(R.id.editText7);
         final String message3 = editText3.getText().toString();
-        String pesan=hasil.concat("-");
+        pesan=hasil.concat("-");
         pesan=pesan.concat(message2);
         pesan=pesan.concat("-");
         pesan=pesan.concat(message3);
@@ -66,9 +74,32 @@ public class RegisActivity extends AppCompatActivity {
             }
         }
         else{
-            Intent intent = new Intent(RegisActivity.this, VerifActivity.class);
-            intent.putExtra(EXTRA_MESSAGEEE,pesan);
-            startActivity(intent);
+            database.child("Users")
+                    .orderByChild("notelp")
+                    .equalTo(hasil)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String clubkey="";
+
+                            for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
+                                clubkey = childSnapshot.getKey();
+                            }
+                            if (clubkey.equals("")){
+                                editText.setError("Nomor Telepon Sudah terdaftar");
+                            }
+                            else{
+                                Intent intent = new Intent(RegisActivity.this, VerifActivity.class);
+                                intent.putExtra(EXTRA_MESSAGEEE,pesan);
+                                startActivity(intent);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            editText.setError("Nomor Telepon Sudah terdaftar");
+                        }
+                    });
         }
     }
     public void Balik(View arg0) {
